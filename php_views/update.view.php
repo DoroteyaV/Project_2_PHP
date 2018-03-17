db2943f8fe15bfddbc75a6455eb113ef6777ca75
Emil <emilsergiev@abv.bg>
2018-03-17 17:26:55 +0200

    fixed update form

--- php_views/update.view.php
+++ php_views/update.view.php
@@ -1,21 +1,29 @@
 <?php include 'php_includes/header.php'; ?>
 
-<form action="update.php" method="post">
-	<label for="task_title">
-			Task title:
-			<input type="text" name="task_title" id="task_title" value="<?= $task['task_title']?>"
-			placeholder="Task title...">
-		</label>
-		<label for="task_description">
-			Task description:
-			<input type="text" name="task_description" id="task_description" value="<?= $task['task_description']?>" placeholder="Task description...">
-		</label>
-		<label for="due_date">
-			Insert due date (format "yyyy-m-d"):
-			<input type="text" name="due_date" id="due_date" value="<?= $task['due_date']?>" placeholder="Task due date...">
-		</label>
-		<input type="hidden" name="id" value="<?= $task['id']?>" >
-		<input type="submit" name="submit" value="Update">
-</form>
+<h1 class="text-center">Update Task <?= $task['task_title']?></h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 well">
+
+            <form action="create.php" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input id="title" type="text" class="form-control" name="task_title" value="<?= $task['task_title']?>" required>
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea id="description" rows="5" class="form-control" name="task_description" required><?= $task['task_description']?></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="date">Due Date (format "yyyy-mm-dd")</label>
+                    <input id="date" class="form-control" name="due_date" value="<?= $task['due_date']?>" required>
+                </div>
+                <input class="btn btn-primary btn-block" type="submit" name="submit" value="Create">
+            </form>
+
+        </div>
+    </div>
+</div>
 
 <?php include 'php_includes/footer.php'; ?>
